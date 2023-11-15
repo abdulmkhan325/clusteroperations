391377a1944b76f992680e63bfbc593c60d9b99b
abdulmkhan325 <abdulmkhan325@gmail.com>
2023-11-15 23:42:19 +1100

    updated createcluster.sh

--- ansible/roles/scripts/createcluster.sh
+++ ansible/roles/scripts/createcluster.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
-#Get Credentials 
+# Credentials File Path
+credentials_file="../../../.credentials" 
+token="" 
+
+# Get Credentials
 readCredentials() {
-
-
+    echo " -> reading credentials..."
+    if [ -e "$credentials_file" ]; then
+        token=$(<"$credentials_file") 
+        echo -e "The .credentials file exists. \n"
+        echo "- - - T - O - K - E - N - - - "
+        echo "$token"
+        echo "- - - - - - - - - - - - - - - "
+    else
+        echo "The .credentials file does not exist."
+    fi
 }
 
+# Login using token and create cluster 
+createCluster() {
+    echo -e "\n -> verifying credentials \n" 
 
+    rosa login --token="$token" || { echo "ROSA login failed"; exit 1; }
+    
+    rosa verify quota || { echo "ROSA verify quota failed"; exit 1; }
+    
+    rosa create account-roles --mode auto || { echo "ROSA account creation failed"; exit 1; }
 
+    echo -e "\n\n -> Enter your cluster name: " 
+    read cname
 
-#Check Plugin 
-if eval "rosa" > /dev/null ; then 
+    rosa create cluster --cluster-name $cname --sts --mode auto 
+
+    # check exit status of the commands 
+    if [ $? -eq 0 ]; then
+        echo -e "\n -> ROSA creating cluster $cname \n" 
+        rosa logs install -c $cname --watch
+    else
+        echo "ROSA cluster creation failed with exit status $?."
+    fi
+}
+
+# Check if rosa command is available 
+if command -v rosa > /dev/null; then
     echo "rosa found!"
-    readCredentials
+    readCredentials  
+    #createCluster 
 else
+    echo "rosa not found. Exiting..."
     exit 1
 fi
-
-#Rosa commands for creation
-
+ 
